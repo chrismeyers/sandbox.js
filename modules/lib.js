e45be6aed3405b591bc0ed2d561a0669bc6e4fe8
Chris Meyers <cm.02.93@gmail.com>
2020-10-08 22:53:09 -0600

    Added ECMAScript modules example

---
+++ modules/lib.js
@@ -0,0 +1,4 @@
+const PI = 3.14159;
+const areaOfCircle = (r) => PI * r ** 2;
+
+export { PI, areaOfCircle };
---
+++ modules/main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
+import { areaOfCircle } from './lib.js';
+
+console.log(areaOfCircle(3));
