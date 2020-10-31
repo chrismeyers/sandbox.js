f37c1daeaf27739aaf19d5f4f056d10978ccd34b
Chris Meyers <cm.02.93@gmail.com>
2020-10-30 22:29:38 -0600

    Expanded package.json of es-modules example

--- es-modules/main.js
+++ es-modules/main.js
@@ -2,5 +2,6 @@
 /* eslint-disable no-console */
 import area, { circumference } from './circle.js';
 
-console.log(area(3));
-console.log(circumference(3));
+const radius = 3;
+console.log(`Circle radius = ${radius}, area = ${area(3)}`);
+console.log(`Circle radius = ${radius}, circumference = ${circumference(3)}`);
